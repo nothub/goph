7751d0c73f82755dc89b8a0248c956fb70d1269f
Florian Hübner <fhuebner@posteo.de>
2024-08-26 16:49:41 +0200

    test flakiness test

---
+++ scripts/flaky.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+set -eu
+cd "$(dirname "$(realpath "$0")")/.."
+
+failed=0
+total=10
+
+i=1
+while [ "$i" -le "${total}" ]; do
+
+    go clean -testcache
+
+    set +e
+    go test -v -vet='all' .
+    status="$?"
+    set -e
+
+    case ${status} in
+        0) ;;
+        1) failed="$(expr "${failed}" + 1)" ;;
+        *)
+            echo "Failed with status: $?"
+            exit 1
+            ;;
+    esac
+
+    i=$(expr "$i" + 1)
+done
+
+printf "failed %s/%s times\n" "${failed}" "${total}"
